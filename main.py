304af51791d5faeb5e35bac2955cf5456339922f
Dinesh <dineshveguru123@gmail.com>
2022-03-14 20:39:33 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+def sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        l = arr[:mid]
+        r = arr[mid:]
+        sort(l)
+        sort(r)
+        merge(l, r, arr)
+    return arr
+
+
+def merge(left, right, result):
+    i = 0
+    j = 0
+    k = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result[k] = left[i]
+            i = i + 1
+            k = k + 1
+        else:
+            result[k] = right[j]
+            k = k + 1
+            j = j + 1
+    while i < len(left):
+        result[k] = left[i]
+        k = k + 1
+        i = i + 1
+    while j < len(right):
+        result[k] = right[j]
+        k = k + 1
+        j = j + 1
+    return result
+
+
+print(sort([8, 9, 4, 1, 1, 4, 6, 7, 6, 7, 9, 1, 2]))
+
