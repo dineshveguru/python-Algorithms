4b92c6a072e7431971461ea653c10d229bde558f
Dinesh <dineshveguru123@gmail.com>
2022-03-20 12:35:23 +0530

    quick sort was done

--- quickSort.py
+++ quickSort.py
@@ -1,10 +1,21 @@
 a = [1, 1, 1, 8, 2]
 
 
-def swap(arr, a, b):
+# taken an initial array
+
+
+def swap(arr, a, b):  # used for swapping two integers with their indexes in array
     temp = arr[a]
     arr[a] = arr[b]
     arr[b] = temp
+
+
+"""partioning the array such that the pivot element 
+comes to its correct position in array and
+all the elements less than pivot
+comes to right side of pivot and 
+all the elements greater than pivot comes to left side
+of pivot"""
 
 
 def partition(arr, start, end):
@@ -21,8 +32,8 @@ def quicksort(start, end, arr):
     if start >= end:
         return
     boundary = partition(arr, start, end)
-    quicksort(start, boundary - 1, arr)
-    quicksort(boundary + 1, end, arr)
+    quicksort(start, boundary - 1, arr)         #recursively calling the array and sorting the right part of pivot
+    quicksort(boundary + 1, end, arr)           #recursively calling the array and sorting the left part of pivot
 
 
 quicksort(0, len(a) - 1, a)
